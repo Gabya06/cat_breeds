ef43b4aa23e77a99af7ae60e484ffa8c6c7b8203
gabya06 <gabya06@gmail.com>
2025-04-11 12:12:35 -0400

    updating setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,11 @@ setup(
         "requests",
         "chromadb==1.0.3",
         "google-genai==1.7.0",
-        "matplotlib",
-        "Pillow",
-        "openai",
-        "clip",
-        "transformers",
-        "torch",
+        "matplotlib==3.8.0",
+        "Pillow==9.4.0",
+        "openai==0.27.2",
+        "transformers==4.45.2",
+        "torch==2.6.0",
     ],
     entry_points={  # Optionally add CLI commands if needed
         # "console_scripts": [
